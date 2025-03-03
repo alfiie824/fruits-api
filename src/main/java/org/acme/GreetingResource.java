5483c2517d93c0dba0ef8a67859e8c2d60f84ed1
Anna-Lea Fuhrer <anna-lea.fuhrer@dvbern.ch>
2025-03-03 16:00:34 +0100

    init project

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,27 @@
+package org.acme;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<String > hello() throws InterruptedException {
+        return Uni.createFrom().item("Hello");
+    }
+
+    @Path("/fruits")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<List<Fruit>> getFruits() throws InterruptedException {
+        Thread.sleep(5000);
+        return Fruit.listAll();
+    }
+}
